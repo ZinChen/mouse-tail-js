fccb69177cd42e16747328b2c1f6a17977356597
Y.Zinchenko <zinchen777@gmail.com>
2017-11-25 19:22:14 +0300

    change mouse move from document to card

--- src/scripts.js
+++ src/scripts.js
@@ -1,4 +1,19 @@
+'use strict'
+
 var Tilt = function(options) {
+  var speedX,
+      speedY,
+      velocityX,
+      velocityY;
+
+  var handleMouseOver = function() {
+
+  };
+
+  var calcPosFromMiddle = function() {
+
+  };
+
   var card = options.card,
       shine = card.querySelector('.shine'),
       moveOpt = {
@@ -11,6 +26,37 @@ var Tilt = function(options) {
           r: 0
         }
       };
+
+  card.addEventListener('mousemove', function(e) {
+    // console.log(e);
+    var mousepos = getMousePos();
+    var relmousepos = getRelativeMousePos(card, mousepos);
+    console.log(relmousepos);
+  });
+
+  var getRelativeMousePos = function(el, mousepos) {
+    var bounds = el.getBoundingClientRect();
+    var docScrolls = getDocScrolls();
+    var relmousepos = {
+      x: mousepos.x - bounds.left - docScrolls.left,
+      y: mousepos.y - bounds.top - docScrolls.top
+    };
+    var halfX = bounds.width / 2
+    var halfY = bounds.bottom / 2;
+
+    relmousepos.x -= halfX;
+    relmousepos.y -= halfY;
+
+
+    if (Math.abs(relmousepos.x) > halfX) {
+      relmousepos.x = null;
+    }
+
+    if (Math.abs(relmousepos.y) > halfY) {
+      relmousepos.y = null;
+    }
+    return relmousepos;
+  };
 
   var setTransform = function(el, pos, opt) {
     var val = {
@@ -51,15 +97,23 @@ function getMousePos(e) {
     posx = e.pageX;
     posy = e.pageY;
   } else if (e.clientX || e.clientY) {
-    posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
-    posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+    var docScrolls = getDocScrolls();
+    posx = e.clientX + docScrolls.left;
+    posy = e.clientY + docScrolls.top;
   }
   return { x : posx, y : posy };
 }
 
-card = document.querySelector('.content-wrapper');
-tilt = new Tilt({card: card});
+function getDocScrolls() {
+  return {
+    left: document.body.scrollLeft + document.documentElement.scrollLeft,
+    top: document.body.scrollTop + document.documentElement.scrollTop
+  };
+}
 
-document.addEventListener('mousemove', function() {
-  tilt.onMousemove();
-});
+var card = document.querySelector('.content-wrapper');
+var tilt = new Tilt({card: card});
+
+// document.addEventListener('mousemove', function() {
+//   tilt.onMousemove();
+// });
