e48d7b4eea0b2bc2aaafda58b7e02134fd6b3242
Y.Zinchenko <zinchen777@gmail.com>
2017-12-19 22:52:42 +0300

    add svg circle following mouse

--- src/scripts.js
+++ src/scripts.js
@@ -1,107 +1,41 @@
-class Tilt {
+class Tale {
   constructor(options) {
     this.speedX;
     this.speedY;
-    this.velocityX;
-    this.velocityY;
-    this.card = options.card;
-    this.shine = card.querySelector('.shine'),
-    this.moveOpt = {
-      card: {
-        t: 0,
-        r: 0.02
-      },
-      shine: {
-        t: 0.3,
-        r: 0
-      }
-    };
-    this.setMouseEventsOnCard();
+    this.el = options.el;
+    this.mouseCircle = this.el.querySelector('#mouse-circle');
+    this.setMouseEvents();
   }
 
   handleMouseOver() {
 
   }
 
-  calcPosFromMiddle() {
+  setMouseEvents() {
 
-  }
 
-  setMouseEventsOnCard() {
-    this.card.addEventListener('mousemove', e => {
+    this.el.addEventListener('mousemove', e => {
       var mousepos = getMousePos();
-      var relmousepos = this.getRelativeMousePos(this.card, mousepos);
-      console.log(relmousepos);
-      this.setTransform(this.card, relmousepos, this.moveOpt.card);
-      this.setTransform(this.shine, relmousepos, this.moveOpt.shine);
+      var circle = this.mouseCircle;
+      circle.setAttribute("cx", mousepos.x);
+      circle.setAttribute("cy", mousepos.y);
+      console.log(mousepos);
     });
 
-    this.card.addEventListener('mouseleave', e => {
+    this.el.addEventListener('mouseleave', e => {
       console.log('leave :');
       console.log(e);
     });
 
-    this.card.addEventListener('mouseenter', e => {
+    this.el.addEventListener('mouseenter', e => {
       console.log('=> enter :');
       console.log(e);
     });
   }
 
-  getRelativeMousePos(el, mousepos) {
-    var bounds = el.getBoundingClientRect();
-    var docScrolls = getDocScrolls();
-    var relmousepos = {
-      x: mousepos.x - bounds.left - docScrolls.left,
-      y: mousepos.y - bounds.top - docScrolls.top
-    };
-    var halfX = bounds.width / 2;
-    var halfY = bounds.height / 2;
-
-    relmousepos.x -= halfX;
-    relmousepos.y -= halfY;
-
-    if (Math.abs(relmousepos.x) > halfX) {
-      relmousepos.x = null;
-    }
-
-    if (Math.abs(relmousepos.y) > halfY) {
-      relmousepos.y = null;
-    }
-    return relmousepos;
-  }
-
-  setTransform(el, pos, opt) {
-    if ( !pos.x || !pos.y ) {
-      return;
-    }
-
-    var val = {
-      t: {
-        x: pos.x * opt.t,
-        y: pos.y * opt.t
-      },
-      r: {
-        x: -1 * pos.y * opt.r,
-        y: pos.x * opt.r
-      }
-    },
-    transformStyle = [
-      'perspective(1000px)',
-      'translateX(', val.t.x , 'px)',
-      'translateY(', val.t.y, 'px)',
-      'rotateX(', val.r.x, 'deg)',
-      'rotateY(', val.r.y, 'deg)',
-    ];
-    transformStyle = transformStyle.join('');
-
-    el.style.WebkitTransform =
-    el.style.transform = transformStyle;
-  }
-
   onMousemove() {
     var mousepos = getMousePos();
     this.setTransform(card, mousepos, this.moveOpt.card);
-    this.setTransform(shine, mousepos, this.moveOpt.shine);
   }
 }
 
@@ -127,8 +61,8 @@ function getDocScrolls() {
   };
 }
 
-var card = document.querySelector('.content-wrapper');
-var tilt = new Tilt({card: card});
+var el = document.querySelector('#main-svg');
+var tilt = new Tale({el: el});
 
 // document.addEventListener('mousemove', function() {
 //   tilt.onMousemove();
