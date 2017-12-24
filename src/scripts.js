42b392ae19d7c188f08b86e6ca1ef336497cb0a3
Y.Zinchenko <zinchen777@gmail.com>
2017-12-24 12:30:53 +0300

    add first version of calc new tale pos as hypotenuse

--- src/scripts.js
+++ src/scripts.js
@@ -23,19 +23,41 @@ class Tale {
   }
 
 // TODO:
-// Add request animation frame
+// Add request animation frame +
 // Add initial first mouse pos setup for tale
 // Add limit distance to tale
 
+// wait in requestAnimFrame for mousepos is got
+// and then start requestAnimFrame with mousetalefunc
+// Calc hipotenuse in separate method
+
   moveTale() {
     this.talex = this.talex || this.x;
+    this.taley = this.taley || this.y;
+
+
+    if (!window.ctrlPressed) {
+      this.requestId = requestAnimationFrame(this.moveTale.bind(this));
+      return;
+
+    }
+    var taleNewPos = this.calcDirectRoute({
+      x : this.talex,
+      y : this.taley
+    }, {
+      x : this.x,
+      y : this.y
+    }, 3);
+    // console.log(taleNewPos);
+
     var xd = this.x - this.talex;
     var directionx = xd > 0 ? 1 : -1;
+    if (taleNewPos.x) xd = taleNewPos.x;
     this.talex += Math.min(this.speedx, Math.abs(xd)) * directionx;
 
-    this.taley = this.taley || this.y;
     var yd = this.y - this.taley;
     var directiony = yd > 0 ? 1 : -1;
+    if (taleNewPos.y) yd = taleNewPos.y;
     this.taley += Math.min(this.speedy, Math.abs(yd)) * directiony;
 
     var tale = this.mouseTale;
@@ -45,7 +67,61 @@ class Tale {
     this.requestId = requestAnimationFrame(this.moveTale.bind(this));
   }
 
+  calcDirectRoute(posFrom, posTo, length) {
+    var p1 = posFrom;
+    var p2 = posTo;
+
+    // if x eqals then return only y
+    // if y equals then return only x
+    if (p1.x == p2.x && p1.y == p2.y) {
+      return {
+        x : 0,
+        y : 0
+      };
+    } else if (p1.x == p2.x) {
+      return {
+        x : p1.x,
+        y : 0
+      };
+    } else if (p1.y == p2.y) {
+      return {
+        x : 0,
+        y : p1.y
+      };
+    }
+
+    var dxOrigin = p2.x - p1.x; // Math.abs()
+    var dyOrigin = p2.y - p1.y; // Math.abs()
+    var tg = dyOrigin / dxOrigin;
+
+    var sinsq = 1 / (1 + 1/tg * tg);
+    var cossq = 1 - sinsq;
+    var sin = Math.sqrt(sinsq);
+    var cos = Math.sqrt(cossq);
+
+    var dx = length * cos;
+    var dy = length * sin;
+
+    // console.log({p1: p1, p2: p2});
+    // console.log("tg: " + tg);
+    // console.log("sinsq:" + sinsq);
+    // console.log("cossq:" + cossq);
+    // console.log("dx: " + dx);
+    // console.log("dy: " + dy);
+
+    return {
+      x: dx,
+      y: dy
+    };
+  }
+
   setMouseEvents() {
+    window.onkeydown = function(e) {
+      this.ctrlPressed = true;
+    };
+    window.onkeyup = function(e) {
+      this.ctrlPressed = false;
+    };
     this.el.addEventListener('mousemove', e => {
       var mousepos = getMousePos();
       var circle = this.mouseCircle;
@@ -54,8 +130,8 @@ class Tale {
       // this.moveTaleTo(mousepos);
       this.x = mousepos.x;
       this.y = mousepos.y;
-      console.log(mousepos);
-      console.log(this.talex);
+      // console.log(mousepos);
+      // console.log(this.talex);
     });
 
     this.el.addEventListener('mouseleave', e => {
